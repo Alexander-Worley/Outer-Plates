bb69af47d014a403c3a9e79d279e28b4f20f268c
Alexander-Worley <108299450+Alexander-Worley@users.noreply.github.com>
2024-05-12 00:52:36 -0700

    Defined item pickup range for players
    Added a pickup range that shifts with the player. (i.e. if the player walks to the left, their pickup range will be on their left side)

--- Global/utils.gd
+++ Global/utils.gd
@@ -2,7 +2,7 @@ extends Node
 
 # GLOBAL FUNCTIONS
 # This file contains this project's global functions
-# To access global function "x", call "utils.x" anywhere.
-	# "utils.x" is case-specific
+# To access global function "x", call "Utils.x" anywhere.
+	# "Utils.x" is case-specific
 # example: func saveGame(): ...
 # example: func loadGame(): ...
--- Restaurant/Player/player.gd
+++ Restaurant/Player/player.gd
@@ -2,17 +2,26 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+# Given whether the player is moving up, down, left, right, or diagonal,
+# set the position of their pickup range
+func set_pickup_range_position(horizontal, vertical):
+	horizontal *= Global.PIXEL_DIMENSION
+	vertical *= Global.PIXEL_DIMENSION
+	$pickup_range.position = Vector2(horizontal, vertical)
+
 func _physics_process(delta):
 	# TODO: Replace "ui" variables with custom gameplay actions
 	# Gets the input movements and handles the movement/deceleration.
 	var horizontalMovement = Input.get_axis("ui_left", "ui_right")
+	var verticalMovement = Input.get_axis("ui_up", "ui_down")
 	if horizontalMovement:
 		velocity.x = horizontalMovement * SPEED
+		set_pickup_range_position(horizontalMovement, verticalMovement)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	var verticalMovement = Input.get_axis("ui_up", "ui_down")
 	if verticalMovement:
 		velocity.y = verticalMovement * SPEED
+		set_pickup_range_position(horizontalMovement, verticalMovement)
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 		
