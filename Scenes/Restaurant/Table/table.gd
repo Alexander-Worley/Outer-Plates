3ae0cb4d12d4117a60cac5988de9125994383855
Alexander-Worley <108299450+Alexander-Worley@users.noreply.github.com>
2024-05-14 00:24:09 -0700

    Added slug food spritesheet

--- Scenes/Restaurant/Table/table.gd
+++ Scenes/Restaurant/Table/table.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
+const CENTER_OF_SURFACE = Vector2(0,0)
+
 # Given a holdable, set it on the surface
 func set_holdable_on_surface(holdableInHand: Area2D):
 	add_child(holdableInHand)
 	holdableInHand.name = "holdableOnSurface"
-	$holdableOnSurface.position = Vector2(0,0)
+	$holdableOnSurface.position = CENTER_OF_SURFACE
