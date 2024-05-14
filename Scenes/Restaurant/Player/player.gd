c4b1baca515805f98d152e49bbd15d5ae4286a5b
Alexander-Worley <108299450+Alexander-Worley@users.noreply.github.com>
2024-05-14 02:51:04 -0700

    Final Prototype 1 Changes

--- Scenes/Restaurant/Player/player.gd
+++ Scenes/Restaurant/Player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 const SPEED: float = 300.0
+var isHolding: bool = false
 var holdablesInRange: Array = []
 var surfacesInRange: Array = []
 
@@ -11,6 +12,7 @@ func pickup_holdable(holdable: Area2D):
 	$interact_range.add_child(holdableInHand)
 	$interact_range/holdableInHand.position = Vector2(0,0)
 	holdable.queue_free()
+	isHolding = true
 
 # Places "holdableInHand" on a surface
 func place_holdable():
@@ -18,6 +20,7 @@ func place_holdable():
 		var surface: Area2D = surfacesInRange.pick_random()
 		surface.set_holdable_on_surface($interact_range/holdableInHand)
 		$interact_range/holdableInHand.queue_free()
+		isHolding = false
 
 # Given whether the player is moving up, down, left, right, or diagonal,
 # set the position of their pickup range
@@ -51,7 +54,7 @@ func _input(event):
 	# TODO: Replace "ui" variables with custom gameplay actions
 	# TODO: Replace "ui_accept with another keystroke
 	if event.is_action_pressed("ui_accept"):
-		if $interact_range/holdableInHand: place_holdable()
+		if isHolding: place_holdable()
 		else: if holdablesInRange:
 			# TODO: Replace "pick_random()" with static decisions.
 				# Perhaps the item most inside of "pickup_range"?
