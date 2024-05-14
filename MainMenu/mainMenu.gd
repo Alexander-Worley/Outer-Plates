75e702ba5077f7722cf19d80b29a2b81531fcb4f
Alexander-Worley <108299450+Alexander-Worley@users.noreply.github.com>
2024-05-13 17:51:33 -0700

    Replaced play button with updated start button

--- MainMenu/mainMenu.gd
+++ MainMenu/mainMenu.gd
@@ -5,5 +5,6 @@ func _on_quit_pressed():
 	get_tree().quit()
 
 # Changes to the Restaurant Scene
-func _on_play_pressed():
+func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Restaurant/restaurant.tscn")
+	
