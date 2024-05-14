717fa97da1d35eaa7cbab062da5f4163860fc2f7
Alexander-Worley <108299450+Alexander-Worley@users.noreply.github.com>
2024-05-14 00:31:11 -0700

    Adjusted food position on table

--- Scenes/Restaurant/Table/table.gd
+++ Scenes/Restaurant/Table/table.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-const CENTER_OF_SURFACE = Vector2(0,0)
+const CENTER_OF_SURFACE = Vector2(0,-8)
 
 # Given a holdable, set it on the surface
 func set_holdable_on_surface(holdableInHand: Area2D):
