47ba9bf966ae709c2307f0db91aab58ad1d6b163
Alexander-Worley <108299450+Alexander-Worley@users.noreply.github.com>
2024-05-17 16:59:36 -0700

    Surfaces now check if they are already holding a holdable before accepting a new one

--- Scenes/Restaurant/Table/table.gd
+++ Scenes/Restaurant/Table/table.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
 const CENTER_OF_SURFACE = Vector2(0,-8)
+var isHolding: bool = false
 
 # Given a holdable, set it on the surface
 func set_holdable_on_surface(holdableInHand: Area2D):
+	if isHolding: return false
 	var holdableOnSurface: Area2D = holdableInHand.duplicate()
 	holdableOnSurface.name = "holdableOnSurface"
 	add_child(holdableOnSurface)
 	$holdableOnSurface.position = CENTER_OF_SURFACE
+	isHolding = true
+	return true
