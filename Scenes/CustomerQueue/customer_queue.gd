143d99052b96057eb9eb9f2cad2e42ef8929a346
ThatMatt <mattjv22@stanford.edu>
2024-05-20 00:51:29 -0700

    Introduced basic customer queue functionality

--- Scenes/CustomerQueue/customer_queue.gd
+++ Scenes/CustomerQueue/customer_queue.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
-const CUSTOMER_QUEUE_SIZE = 3
-var customer_queue = [false, false, false]
-var first_spot_free = 0
+const MAX_QUEUE_SIZE: int = 3
+const NO_SPOT_FREE: int = -1
+const INTER_CUST_SPACE: int = 6
+const CUSTOMER_WIDTH: int = 32
+
+
+var customer_queue: Array = []
+var first_spot_free: int = 0
 var customer_scene = preload("res://Scenes/Restaurant/Customer/customer.tscn")
 
 # Called when the node enters the scene tree for the first time.
@@ -15,16 +20,18 @@ func _process(delta):
 	pass
 
 
-func _on_timer_timeout():
-	var customer = customer_scene.instance()
-	
-	# customer.position = Vector2() SET THE POSITION
-	
-	
 func get_open_spot():
-	# Yes, you can do a ternary.
-	if first_spot_free >= CUSTOMER_QUEUE_SIZE:
-		return -1
+	# You want a ternary, don't you? Do it then. You won't.
+	if first_spot_free >= MAX_QUEUE_SIZE:
+		return NO_SPOT_FREE
 	return first_spot_free
 
 
+func _on_customer_spawn_timer_timeout():
+	var spot = get_open_spot()
+	if spot == NO_SPOT_FREE:
+		return
+	var customer = customer_scene.instantiate()
+	customer.position = Vector2(position.x + spot * (CUSTOMER_WIDTH + INTER_CUST_SPACE), position.y)
+	add_child(customer)
+	first_spot_free += 1
