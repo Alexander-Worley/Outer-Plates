edeb6203b002c6bbbcfdac80108f39797bdc0b95
Alexander-Worley <108299450+Alexander-Worley@users.noreply.github.com>
2024-05-21 22:52:30 -0700

    Surfaces with multiple holdables now display them in the correct spots
    Progress on #29

--- Scenes/Restaurant/PlainSurface/plainSurface.gd
+++ Scenes/Restaurant/PlainSurface/plainSurface.gd
@@ -4,9 +4,8 @@ extends Area2D
 # A 2D Array: [Vector2][bool]
 # Vector2 contains a surfaceCenter
 # bool contains whether said surfaceCenter is occupied
-var centersOfSurface: Array[Vector2]= []
+var centersOfSurface: Array = []
 var holdablesOnSurface: Array[Area2D] = []
-var maxHoldables: int = 1
 @export_category("Developer Tools :0")
 @export_enum("Up", "Right", "Down", "Left") var direction: int = 0
 @export var width: int = 1
@@ -15,7 +14,7 @@ var maxHoldables: int = 1
 
 func _ready():
 	# Set maxHoldables
-	maxHoldables = width * height
+	var maxHoldables: int = width * height
 	# Set Texture
 	if texture: $Surface.texture = texture
 	# Set Collision
@@ -25,48 +24,60 @@ func _ready():
 	$Surface.rotation_degrees = 90 * direction
 	# Set centersOfSurface
 	set_surface_points()
+	# Initialize holdablesOnSurface
+	for i: int in maxHoldables:
+		holdablesOnSurface.append(null)
 	# Set starting holdablesOnSurface[0] if needed
 	var originalHoldables: Array[Node] = find_children("*", "Area2D", false)
 	for i: int in originalHoldables.size():
-		# If this errors, you have too many holdables on one surface!
-		originalHoldables[i].position = centersOfSurface[i]
-		holdablesOnSurface.append(originalHoldables[i])
+		# If this errors, you spawned too many holdables on one surface!
+		originalHoldables[i].position = centersOfSurface[i][0]
+		centersOfSurface[i][1] = true
+		holdablesOnSurface[i] = originalHoldables[i]
 
 func set_surface_points():
-	var w_offset = (width - 1) * 16
-	var h_offset = (height - 1) * 16
+	var w_offset = (width - 1) * (Global.PIXEL_DIMENSION / 2.0)
+	var h_offset = (height - 1) * (Global.PIXEL_DIMENSION / 2.0)
+	# Set every point
 	for w in width:
 		for h in height:
-			var newPoint = Vector2(w * 32 - w_offset, h * 32 - h_offset)
+			var newX = w * Global.PIXEL_DIMENSION - w_offset
+			var newY = h * Global.PIXEL_DIMENSION - h_offset
+			var newPoint = Vector2(newX, newY)
 			# Adjust for surface rotation
 			match direction:
 				0:
-					newPoint.y -= 8
+					newPoint.y -= Global.PIXEL_DIMENSION / 4.0
 				1:
-					newPoint.x += 8
+					newPoint.x += Global.PIXEL_DIMENSION / 4.0
 				2:
-					newPoint.y += 8
+					newPoint.y += Global.PIXEL_DIMENSION / 4.0
 				3:
-					newPoint.x -= 8
-			centersOfSurface.append(newPoint)
+					newPoint.x -= Global.PIXEL_DIMENSION / 4.0
+			centersOfSurface.append([newPoint, false])
 
 # Given a holdable, set it on the surface
 # Return true if successful and false if not successful
-# This wrapper function is necessary because interactable surfaces (like Stoves) have additional functions
+# This wrapper function is necessary because interactable surfaces (like Stoves) have additional functionality
 func set_holdable_on_surface_wrapper(holdableInHand: Area2D):
 	return set_holdable_on_surface(holdableInHand)
 func set_holdable_on_surface(holdableInHand: Area2D):
-	if holdablesOnSurface.size() >= maxHoldables: return false
 	var newHoldable: Area2D = holdableInHand.duplicate()
-	holdablesOnSurface.append(newHoldable)
-	add_child(newHoldable)
-	newHoldable.position = centersOfSurface[holdablesOnSurface.size() - 1]
-	newHoldable.rotation = 0
-	return true
+	for i: int in centersOfSurface.size():
+		if !centersOfSurface[i][1]: # If center has no holdable
+			newHoldable.position = centersOfSurface[i][0]
+			newHoldable.rotation = 0
+			holdablesOnSurface[i] = newHoldable
+			centersOfSurface[i][1] = true # Center is now occupied
+			add_child(newHoldable)
+			return true
 
 # Given a holdable, remove it from the surface
-func remove_holdable_from_surface(holdable):
-	holdablesOnSurface.erase(holdable)
+func remove_holdable_from_surface(holdable: Area2D):
+	for i: int in centersOfSurface.size():
+		if centersOfSurface[i][0] == holdable.position:
+			holdablesOnSurface[i] = null
+			centersOfSurface[i][1] = false # Center is no longer occupied
 
 func _physics_process(_delta):
 	# Update sprite in the editor interface
