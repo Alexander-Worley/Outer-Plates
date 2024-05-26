335b0f9ce5563ffecdba027abdb890e8e8d313f9
Alexander-Worley <108299450+Alexander-Worley@users.noreply.github.com>
2024-05-25 22:25:23 -0700

    Added controller support to the Main Menu
    Closes #49

--- Scenes/MainMenu/mainMenu.gd
+++ Scenes/MainMenu/mainMenu.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+@onready var startButton = $startButton
+
 # Changes to the Restaurant Scene
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://DevScenes/DevMenu.tscn")
+
+func _input(event):
+	if event.is_action_pressed("pickup") or event.is_action_pressed("interact"):
+		startButton.button_pressed = true
+		await get_tree().create_timer(0.1).timeout
+		_on_start_button_pressed()
