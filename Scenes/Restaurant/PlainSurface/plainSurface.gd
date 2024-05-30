89d4be110b9827db0b1514fb827a41a8e232bf98
Alexander-Worley <108299450+Alexander-Worley@users.noreply.github.com>
2024-05-30 14:09:45 -0700

    Fixes a bug in which the "Doneness" property of cookable holdable was lost when placed on a surface
    Closes #84

--- Scenes/Restaurant/PlainSurface/plainSurface.gd
+++ Scenes/Restaurant/PlainSurface/plainSurface.gd
@@ -67,6 +67,7 @@ func set_holdable_on_surface_wrapper(holdableInHand: Area2D):
 	return set_holdable_on_surface(holdableInHand)
 func set_holdable_on_surface(holdableInHand: Area2D):
 	var newHoldable: Area2D = holdableInHand.duplicate()
+	if holdableInHand.is_in_group("Cookable"): newHoldable.doneness = holdableInHand.doneness
 	for i: int in centersOfSurface.size():
 		if !centersOfSurface[i][1]: # If center has no holdable
 			newHoldable.position = centersOfSurface[i][0]
