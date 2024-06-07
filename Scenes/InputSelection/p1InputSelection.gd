8265a2ef050e7b3ec532e0c9c840d8f2d4892efb
Alexander-Worley <108299450+Alexander-Worley@users.noreply.github.com>
2024-06-07 04:33:05 -0700

    Starting Multiplayer Ready Up Scene
    Progress #188

--- Scenes/InputSelection/p1InputSelection.gd
+++ Scenes/InputSelection/p1InputSelection.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
-const playerSelection = preload("res://Scenes/InputSelection/playerSelection.tscn")
-
 func _on_keyboard_input_zone_body_entered(body):
 	Global.isAcceptAllInput = false
 	Global.isP1UsingKeyboard = true
-	Utils.setScene(playerSelection)
-
+	var timer = get_tree().create_timer(0.1)
+	if not timer.is_connected("timeout", Callable(self, "_on_timer_timeout")):
+		timer.connect("timeout", Callable(self, "_on_timer_timeout"))
 
 func _on_controller_input_zone_body_entered(body):
 	Global.isAcceptAllInput = false
 	Global.isP1UsingKeyboard = false
+	var timer = get_tree().create_timer(0.1)
+	if not timer.is_connected("timeout", Callable(self, "_on_timer_timeout")):
+		timer.connect("timeout", Callable(self, "_on_timer_timeout"))
+
+# This timer is necessary to safely unload everything from the scene
+func _on_timer_timeout():
+	const playerSelection = preload("res://Scenes/InputSelection/playerSelection.tscn")
 	Utils.setScene(playerSelection)
