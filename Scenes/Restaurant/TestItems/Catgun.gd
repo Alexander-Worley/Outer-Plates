8f59f83920bbfc169b3320473a85590e4d0d6ee1
Percipi0 <86848675+Percipi0@users.noreply.github.com>
2024-06-08 14:15:15 -0700

    gun tweaks & bug fixes - they fire correctly now in all directions

--- Scenes/Restaurant/TestItems/Catgun.gd
+++ Scenes/Restaurant/TestItems/Catgun.gd
@@ -32,10 +32,11 @@ func shootHelper():
 	b.start($Muzzle.global_position, $Muzzle.global_rotation)
 	get_tree().root.add_child(b)
 	ammo = ammo - 1
-	updateAmmoCounter()
+	#updateAmmoCounter()
 	
 
 func updateAmmoCounter():
+	print(self.get_path())
 	var ammoCount = get_node("/root/Logan/Weapons/ammoCount")
 	ammoCount.text = "Ammo: " + str(ammo)
 
